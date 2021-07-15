4fe1b7b26e06bcdbe5ef22575376312bbed801aa
Maciej <maciejgrochowski93@o2.pl>
2021-07-15 23:43:56 +0200

    the first commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM openjdk:8u191-alpine3.9
+ADD target/dockerLesson2-0.0.1-SNAPSHOT.jar .
+CMD java -jar dockerLesson2-0.0.1-SNAPSHOT.jar
+
+
+
+
+
+
+#FROM openjdk:8u191-alpine3.9
+#COPY . /app
+#WORKDIR /app
+#CMD java -jar dockerLesson2-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/maciej/grochowski/dockerLesson2/DockerLesson2Application.java
@@ -0,0 +1,13 @@
+package maciej.grochowski.dockerLesson2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerLesson2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerLesson2Application.class, args);
+	}
+
+}
